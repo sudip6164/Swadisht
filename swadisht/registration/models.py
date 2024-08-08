794ad313358bfa474b6bb76857e95d3a6499b702
sanziv9999 <sanzivmagat80@gmail.com>
2024-08-08 19:41:46 +0545

    RegistrationModel

--- swadisht/registration/models.py
+++ swadisht/registration/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class registrationFPage(models.Model):
+    restaurantName = models.TextField(max_length=100)
+    ownerName =models.TextField(max_length=50)
+    email = models.EmailField()
+    phone = models.TextField(max_length=20)
+    address= models.TextField(max_length=200)
+    cuisineType= models.TextField(max_length=100)
+    businessLicenceNo=models.TextField(max_length=100)
+    panCardNumber = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.ownerName
+
+
+
